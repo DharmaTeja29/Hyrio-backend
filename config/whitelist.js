1851d13b617ad6100a2499cc8c1f3a0ca45598df
Dharma Teja <1234nelluridharma@gmail.com>
2025-01-31 00:03:46 +0530

    Update whitelist.js

--- config/whitelist.js
+++ config/whitelist.js
@@ -2,6 +2,7 @@ const whitelist = [
     'https://www.google.com',
     'http://localhost:3500',
     'http://localhost:3000',
+    'https://hyrio-backend.onrender.com:10000'
 ];
 
 module.exports = whitelist;
